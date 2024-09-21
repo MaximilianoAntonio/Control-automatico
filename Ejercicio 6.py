ec6e022624f1eda6d5fd52294f26db6478d7fbb5
Maximiliano <maximilianogaetepizza@gmail.com>
2024-09-21 18:48:36 -0300

    arreglado ejercicio 6

--- Ejercicio 6.py
+++ Ejercicio 6.py
@@ -2,9 +2,9 @@ import control as ctrl
 import matplotlib.pyplot as plt
 
 # Parámetros del sistema
-k = 0.04  # Valor de k determinado
+k = 0.2  # Valor de k determinado
 num = [16]
-den = [1, 0.8, 16*k]
+den = [1, (16*k+0.8), 16]
 
 # Crear el sistema en lazo cerrado
 sys = ctrl.TransferFunction(num, den)
@@ -15,12 +15,12 @@ t, y = ctrl.step_response(sys)
 # Graficar la respuesta
 
 plt.plot(t, y, label='Respuesta al escalón unitario')
-plt.title('Respuesta al escalón unitario (k = 0.04)')
+plt.title('Respuesta al escalón unitario (k = 0.2)')
 plt.xlabel('Tiempo (s)')
 plt.ylabel('Respuesta')
-plt.axvline(x=2.25, color='red', linestyle='--', label='Tiempo de levantamiento (Tr) 2.25 [s]')
-plt.axvline(x=4.55, color='green', linestyle='--', label='Tiempo peak (Tp) 4.55 [s]')
-plt.axvline(x=10, color='purple', linestyle='--', label='Tiempo de asentamiento (Ts) 10 [s]')
+plt.axvline(x=0.604, color='red', linestyle='--', label='Tiempo de levantamiento (Tr) 0.604 [s]')
+plt.axvline(x=0.907, color='green', linestyle='--', label='Tiempo peak (Tp) 0.907 [s]')
+plt.axvline(x=2, color='purple', linestyle='--', label='Tiempo de asentamiento (Ts) 2 [s]')
 plt.plot([], [], ' ', label='Sobrepaso 16.3%')
 plt.grid()
 plt.legend() 
